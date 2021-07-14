d93761ec64ffa4a713a5fd848ef23cf956f31ab2
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-07-14 13:30:15 +0200

    Create minimal Flask application along with tests

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<p>Hello, World!</p>'
+
+
+@app.route('/github-actions')
+def hello_github_actions():
+    return '<p>Hello, Github Actions!</p>'
+
+
+@app.route('/digital-ocean')
+def hello_digital_ocean():
+    return '<p>Hello, Digital Ocean!</p>'
---
+++ test_hello.py
@@ -0,0 +1,30 @@
+import pytest
+from hello import app as flask_app
+
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, World!' in response.data
+
+
+def test_hello_github_actions(client):
+    response = client.get('/github-actions')
+    assert response.status_code == 200
+    assert b'Hello, Github Actions!' in response.data
+
+
+def test_hello_digital_ocean(client):
+    response = client.get('/digital-ocean')
+    assert response.status_code == 200
+    assert b'Hello, Digital Ocean!' in response.data
